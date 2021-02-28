f7048c1fb0e8e476ea59d868923f43234526d8fd
aligivili <79777824+aligivili@users.noreply.github.com>
2021-02-28 10:25:32 -0800

    Create 01-Points.py

---
+++ 01-Points.py
@@ -0,0 +1,8 @@
+#How to print double quotes with the string variable in Python?
+StringVar = 'Hello Ali Givili'
+print("\"%s\""% StringVar )
+print("\\%s\\"% StringVar )
+print('"%s"' % StringVar )
+print('"{}"'.format(StringVar))
+print('"%s"' % 'Ali Givili' )
+print('"Ali Givili"' )
